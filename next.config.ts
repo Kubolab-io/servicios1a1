b6a910d5e6f058444a69227d2d001252e5b15daf
Juanes <hola@kubolab.io>
2025-12-15 23:25:45 -0500

    Fix: Use basePath only in production

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@
 
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/servicios1a1',
-  assetPrefix: '/servicios1a1',
+  basePath: isProd ? '/servicios1a1' : '',
+  assetPrefix: isProd ? '/servicios1a1' : '',
   trailingSlash: true,
 };
 
